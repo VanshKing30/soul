7459d8a82f7cf93f4de5d70c1f7d52e59708003f
Vansh <vanshwaldeo360@gmail.com>
2024-01-30 23:33:38 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='parent'>
+      <h1>Hello Jiii</h1>
+      <p>Vansh Waldeo</p>
+      <p>Internet Speed </p>
+    </div>
+  );
+}
+
+export default App;
