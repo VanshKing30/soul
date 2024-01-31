1a162abbc43f9c039197ac78081db6007ad363b7
Vansh <vanshwaldeo360@gmail.com>
2024-01-31 17:03:35 +0530

    New Footer added within new branch

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import './Footer.css';
+
+function Footer(){
+    return(
+        <div className="Editing">
+            <h2>Footer Branch</h2>
+            <p>This is my footer and this work is going on another branch.</p>
+        </div>
+    );
+}
+
+export default Footer;
