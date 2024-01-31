b4fbe796617923c2b41539709428278d71f0c3ea
Vansh <vanshwaldeo360@gmail.com>
2024-01-31 17:06:48 +0530

    Added a new div in footer.jsx

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -7,6 +7,11 @@ function Footer(){
         <div className="Editing">
             <h2>Footer Branch</h2>
             <p>This is my footer and this work is going on another branch.</p>
+
+            <div className="links">
+                <a href="www.google.com">This is my google line</a>
+                <a href="www.instagram.com">This is my link to instagarm</a>
+            </div>
         </div>
     );
 }
