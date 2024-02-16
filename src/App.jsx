453124d56d9fd8e413442e1c167b28c3d8dba150
Vansh <vanshwaldeo360@gmail.com>
2024-02-17 00:11:03 +0530

    Trying out quill lib

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,10 @@
-import './App.css';
-import Navbar from './Components/Navbar';
-import Footer from './Components/Footer';
-import Content from './Components/Content';
 
-function App() {
-  return (
-    <div className='parent'>
 
-      <Navbar/>
-      <h1>Hello Jiii</h1>
-      <p>Vansh Waldeo</p>
-      <p>Internet Speed </p>
-      <h2>Microsoft is hiring freshers.</h2>
+function App(){
 
-      <Content/>
+  return(
 
-      <p>Github pe code upload krdiya hai , Vaise</p>
-
-      <Footer/>
-    </div>
+    <h1>Text Editor</h1>
   );
 }
 
