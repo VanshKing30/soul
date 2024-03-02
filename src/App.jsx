35ecc117d5d198a60b98ad083b6f19d67bccef5a
Vansh <vanshwaldeo360@gmail.com>
2024-03-03 00:02:56 +0530

    Made some routing changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
-import TextEditor from "./Components/TextEditor";
+import './App.css';
+import { Link, Route , Routes } from 'react-router-dom';
 
 function App(){
 
   return(
+    <div className="parent">
 
-    <div className="Parent">
-      <h1>Text Editor</h1>
-      <TextEditor/>
+      <div className="navigation">
+
+        <ul>
+          <li><Link to = "/"> HomePage</Link></li>
+          <li> <Link to = "/players"> Player List </Link>    </li>
+        </ul>
+      </div>
+
+      <Routes>
+        <Route path="/" element = {<h1>Home Page Baby</h1>}></Route>
+        <Route path="/players" element = {<h1>This is the list of Grade a Players</h1>}></Route>
+      </Routes>
     </div>
-
   );
 }
+
+
 
 export default App;
